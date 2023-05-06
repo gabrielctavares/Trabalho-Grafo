981f1d63156df4652262cc31abaa7edd6ea9d260
Gabriela Singulani <gabriela.singulani@ice.ufjf.br>
2023-05-06 09:33:31 -0300

    Modificação de comentários em Grafo.h

--- include/Grafo.h
+++ include/Grafo.h
@@ -15,13 +15,14 @@ class Grafo
         ~Grafo();
 
         void addNo(int id); //adiciona nó com o id informado
-        void removeNo(int id); //remove o nó com o id informado (ainda não implementado)
+        void removeNo(int id); //remove o nó com o id informado
 
-        void addArco(int orig, int dest, float p);
-        void removeArco(int idNo, int idDest);
-        //aqui ainda precisa da função de get e set da aresta
+        void addArco(int orig, int dest, float p); //adiciona um arco do nó com id orig indo no nó com id dest
+        void removeArco(int idNo, int idDest); //remove um arco do nó com id idNo indo no nó com id idDest
 
         bool ehAdjacente(No* x, No* y); // TODO: verificar se é isso mesmo
+        //Gabi: acho melhor usar o id do nó, pois a main não deve ter acesso a endereço de nó
+        //bool ehAdjacente(int id1, int id2);
 
         void vizAberto(); //TODO: colocar os parametros/retorno da função
         void vizFechado(); //TODO: colocar os parametros/retorno da função
@@ -36,8 +37,9 @@ class Grafo
         bool ehBipartido();
         void caminhoMinimo();
         void grauGrafo();
-        void fechoTransDir();
-        void fechoTransInd();
+
+        void fechoTransDir(int idNo); //retorna conjunto de nós que podem ser "alcançados" a partir do nó com id idNo (a ser implementado)
+        void fechoTransInd(int idNo); //retorna conjunto de nós que podem "alcançar" o nó com id idNo (a ser implementado)
         void seqGraus();
         void subgrafoInd();
         void complementGrafo();
