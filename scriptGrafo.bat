10c1f4a2105103ae951bc6e3c556efc3981fb559
Gabriel <gabrielctavares.dev@gmail.com>
2023-07-07 09:51:51 -0300

    Saida do programa

--- scriptGrafo.bat
+++ scriptGrafo.bat
@@ -2,11 +2,12 @@
 
 setlocal enabledelayedexpansion
 
-for %%F in (.\Arquivos_entrada\*) do (
-  set "nomeArquivo=%%~nxF"
-  echo Arquivo: !nomeArquivo!
+for /L %%N in (1,1,5) do (
 
-  for /L %%N in (1,1,10) do (
+  for %%F in (.\Arquivos_entrada\*) do (
+    set "nomeArquivo=%%~nxF"
+    echo Arquivo: !nomeArquivo!
+
     echo Repeticao: %%N
     set "variavelSaida=saida_%%N_!nomeArquivo!"
     .\execGrupoX .\Arquivos_entrada\!nomeArquivo! !variavelSaida! 0 0 1
